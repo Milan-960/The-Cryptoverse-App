ead9a4568b47971eea79fac92806760f5ae0f243
Milan960 <milan.sachani.ms@gmail.com>
2022-05-02 15:58:53 +0200

    api has ben removed

--- src/Api/cryptoApi.js
+++ src/Api/cryptoApi.js
@@ -5,7 +5,7 @@ const baseUrl = "https://coinranking1.p.rapidapi.com/";
 const createRequest = (url) => ({ url, headers: cryptoApiheaders });
 
 const cryptoApiheaders = {
- 
+
 };
 
 export const cryptoApi = createApi({
@@ -19,21 +19,3 @@ export const cryptoApi = createApi({
 });
 
 export const { useGetCryptosQuery } = cryptoApi;
-
-// const options = {
-//   method: "GET",
-//   url: "https://coinranking1.p.rapidapi.com/coins",
-//   params: {
-//     referenceCurrencyUuid: "yhjMzLPhuIDl",
-//     timePeriod: "24h",
-//     "tiers[0]": "1",
-//     orderBy: "marketCap",
-//     orderDirection: "desc",
-//     limit: "50",
-//     offset: "0",
-//   },
-//   headers: {
-//     "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
-//     "X-RapidAPI-Key": "be5696e843msha65bb7bcecb2ed8p1bbd6fjsn37834df93e45",
-//   },
-// };
--- src/Api/cryptoNewsApi.js
+++ src/Api/cryptoNewsApi.js
@@ -2,9 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseUrl = "https://coinranking1.p.rapidapi.com/";
 
-const cryptoNewsHeaders = {
-
-};
+const cryptoNewsHeaders = {};
 
 const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
 
@@ -22,14 +20,3 @@ export const cryptoNewsApi = createApi({
 });
 
 export const { useGetCryptoNewsQuery } = cryptoNewsApi;
-
-// const options = {
-//   method: "GET",
-//   url: "https://bing-news-search1.p.rapidapi.com/news",
-//   params: { safeSearch: "Off", textFormat: "Raw" },
-//   headers: {
-//     "X-BingApis-SDK": "true",
-//     "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
-//     "X-RapidAPI-Key": "be5696e843msha65bb7bcecb2ed8p1bbd6fjsn37834df93e45",
-//   },
-// };
